73cc10a81ef6d32c505474a481f1b5aa2e16595e
Henrik <henrikbreines@gmail.com>
2022-05-15 17:05:45 +0200

    fix

--- dist/lib/utils/constants.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CSGO_ROUND_LENGTH = void 0;
-exports.CSGO_ROUND_LENGTH = 115;
--- dist/lib/utils/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.camelizeIsh = void 0;
-function camelizeIsh(text) {
-    text = text.replace(/[-_\s.]+(.)?/g, (_, c) => (c ? c.toUpperCase() : ""));
-    return text;
-}
-exports.camelizeIsh = camelizeIsh;
